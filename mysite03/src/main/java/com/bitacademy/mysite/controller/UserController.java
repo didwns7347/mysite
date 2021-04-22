89b54dfe18e6e132e8b2a9ead8703439082c9251
'bitacademy-java' <'java.bitacademy@gmail.com'>
2021-04-22 10:48:10 +0900

    user #2

--- mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/bitacademy/mysite/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.bitacademy.mysite.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,13 +23,30 @@ public class UserController {
 
 	@RequestMapping(value="/join", method=RequestMethod.POST)
 	public String join(UserVo vo) {
-		
+		userService.join(vo);
 		return "redirect:/user/joinsuccess";	
 	}
 
-	@RequestMapping("/joinsuccess\"")
+	@RequestMapping("/joinsuccess")
 	public String joinsuccess() {
 		return "user/joinsuccess";	
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String login() {
+		return "user/login";	
+	}
+
+	@RequestMapping(value="/login", method=RequestMethod.POST)
+	public String login(UserVo vo, HttpSession session) {
+		UserVo authUser = userService.getUser(vo);
+		if(authUser == null) {
+			return "redirect:/user/login?result=fail";
+		}
+		
+		/* 로그인 처리 */
+		session.setAttribute("authUser", authUser);
+		return "redirect:/";	
 	}
 	
 }
--- mysite03/src/main/java/com/bitacademy/mysite/service/UserService.java
+++ mysite03/src/main/java/com/bitacademy/mysite/service/UserService.java
@@ -4,10 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bitacademy.mysite.repository.UserRepository;
+import com.bitacademy.mysite.vo.UserVo;
 
 @Service
 public class UserService {
 	
 	@Autowired
-	private UserRepository userRepository; 
+	private UserRepository userRepository;
+
+	public void join(UserVo vo) {
+		userRepository.insert(vo);
+		// 메일 보내기
+	}
+
+	public UserVo getUser(UserVo vo) {
+		return userRepository.findByEmailAndPassword(vo);
+	} 
 }
